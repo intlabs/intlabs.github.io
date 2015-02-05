8be76e96313be49ce769a4c80a3c152f300ba17e
Pete Birley <petebirley@gmail.com>
2015-02-05 20:07:19 +0000

    Update index.js

--- t2/js/index.js
+++ t2/js/index.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	var options = { videoId: 'Hcywf9mwF5U', start: 3 };
+	var options = { videoId: 'Hcywf9mwF5U', mute: false, start: 3 };
 	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
