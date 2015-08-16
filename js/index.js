4db1ee942794ef57271bc3a463cc04647f9f6275
Pete Birley <petebirley@gmail.com>
2015-08-16 23:07:34 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	var options = { videoId: 'eFTLKWw542g', mute: false, start: 0 };
+	var options = { videoId: 'ozyMDcC4F4g', mute: false, start: 0 };
 	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
