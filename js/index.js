ad58c39d6f4578062266850381bab1e545e06845
Pete Birley <petebirley@gmail.com>
2015-08-17 00:03:09 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 $('document').ready(function() {
-	var options = { videoId: 'h37KQu64RY4', mute: false, start: 0 };
+	var options = { videoId: 'ozyMDcC4F4g', mute: false, start: 0 };
 	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
 	// h37KQu64RY4 blobby
 	// ozyMDcC4F4g spit
+	// uoJthzxpn7A Trader Tells Truth
+	//h37KQu64RY4
 });
